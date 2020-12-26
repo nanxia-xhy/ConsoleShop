d2536204d0b4e32e65419fac1e3d61c440311ccf
txh <1973116251@qq.com>
2020-12-26 17:52:27 +0800

    查找Excel并比对用户名和密码

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("请输入用户名:");
+        System.out.print("请输入用户名:");
         Scanner sc =new Scanner(System.in);
         String username =sc.next();
-        System.out.println("你输入的用户名是：" + username);
+        //System.out.println("你输入的用户名是：" + username);
 
-        System.out.println("请输入密码:");
+        System.out.print("请输入密码:");
         String password =sc.next();
-        System.out.println("你输入的密码是：" + password);
+        //System.out.println("你输入的密码是：" + password);
+        File file=new File("C:\\Users\\lenovo\\IdeaProjects\\ConsoleShop\\src\\Users.xlsx");
+        ReadExcel re =new ReadExcel();
+        User users[]=re.readExcel(file);
+        for(int i=0;i<users.length;i++){
+            if(username.equals(users[i].getUsername())&&password.equals(users[i].getPassword())){
+                System.out.println("登陆成功！");
+                break;
+            }
+            else {
+                System.out.println("登陆失败");
+            }
+        }
     }
 }
